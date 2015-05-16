b66960c8ab3d4814fd57d8bf7f91408197219fa5
Boris Wilhelms <mail@wille-zone.de>
2015-05-16 14:32:57 +0200

    Adds psget and posh-git as modules

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,7 @@
+Import-Module PsGet
+
 $psdir="${env:USERPROFILE}\Documents\WindowsPowerShell"  
 Get-ChildItem "${psdir}\*.ps1" -Exclude $MyInvocation.MyCommand.Name | %{.$_} 
 
 # Load posh-git example profile
-
-if (Test-Path "${env:USERPROFILE}\Github\posh-git\profile.example.ps1")
-{
-    . "${env:USERPROFILE}\Github\posh-git\profile.example.ps1"
-}
-
+. '.\Modules\posh-git\profile.example.ps1'
