d586755635cfed244fdea480f5134c5601f65f12
Boris Wilhelms <mail@wille-zone.de>
2015-05-16 14:04:37 +0200

    Adds posh
    Adds posh if it exits in Userprofile\GitHub\posh-git

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,10 @@
 $psdir="${env:USERPROFILE}\Documents\WindowsPowerShell"  
 Get-ChildItem "${psdir}\*.ps1" -Exclude $MyInvocation.MyCommand.Name | %{.$_} 
+
+# Load posh-git example profile
+
+if (Test-Path "${env:USERPROFILE}\Github\posh-git\profile.example.ps1")
+{
+    . "${env:USERPROFILE}\Github\posh-git\profile.example.ps1"
+}
+
