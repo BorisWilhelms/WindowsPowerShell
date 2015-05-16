6ea97f4ae46c653cf492b35a07316480ed3289be
Boris Wilhelms <mail@wille-zone.de>
2015-05-16 20:00:13 +0200

    Fixed path to posh-git module

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,5 +3,4 @@ Import-Module PsGet
 $psdir="${env:USERPROFILE}\Documents\WindowsPowerShell"  
 Get-ChildItem "${psdir}\*.ps1" -Exclude $MyInvocation.MyCommand.Name | %{.$_} 
 
-# Load posh-git example profile
-. '.\Modules\posh-git\profile.example.ps1'
+. (Join-Path (Split-Path $profile) "\Modules\posh-git\profile.example.ps1")
