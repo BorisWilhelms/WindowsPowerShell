deb3026d598e0a8e6749ab7d08297d294e2101d7
Boris Wilhelms <mail@wille-zone.de>
2015-05-16 13:50:11 +0200

    Adds Touch-File.ps1
    Touch-File contains a unix like touch function

---
+++ Touch-File.ps1
@@ -0,0 +1,22 @@
+﻿Function Touch-File
+{
+    param (
+        [string] $file
+    )
+    
+    if($file -eq $null) 
+    {
+        throw "No filename supplied"
+    }
+
+    if(Test-Path $file)
+    {
+        (Get-ChildItem $file).LastWriteTime = Get-Date
+    }
+    else
+    {
+        echo $null > $file
+    }
+}
+
+Set-Alias touch Touch-File
