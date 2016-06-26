f93c1110c6daaf7dd3d98f31f7b5ead0dad7d003
Boris Wilhelms <boris.wilhelms@smarthouse.de>
2016-06-26 21:09:46 +0200

    Create-GlacierArchive creates a zip file and uses Get-FileList to create a csv with the zip content

---
+++ Create-GlacierArchive.ps1
@@ -0,0 +1,7 @@
+﻿function Create-GlacierArchive($folder)
+{
+    $archive = Split-Path -leaf $folder
+    
+    Get-FileList $folder | Export-Csv "$($archive)-Content.csv"
+    Compress-Archive -Path $folder -DestinationPath "$($archive).zip"
+}
