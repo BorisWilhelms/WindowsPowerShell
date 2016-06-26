1e8dd013450b19b33875a05e34205bdecde74a82
Boris Wilhelms <boris.wilhelms@smarthouse.de>
2016-06-26 20:10:59 +0200

    Added Get-FileList script which create a list of files and their MD5 hashs

---
+++ Get-FileList.ps1
@@ -0,0 +1,14 @@
+﻿function Get-FileList
+{
+    ls -Recurse | ForEach-Object {
+        $file = $_
+        $hash = Get-FileHash -Algorithm MD5 $file.FullName
+
+        New-Object -TypeName PSObject -Property @{
+            FileName = Resolve-Path -Relative $file.FullName
+            FileSize = $file.Length
+            LastWriteTime = $file.LastWriteTime
+            Hash = $hash.Hash
+        }
+    }
+}
