7ea05dfde127d12de3da7a5445e3ab53b7bbcf73
Boris Wilhelms <boris.wilhelms@thinktecture.com>
2017-02-19 21:14:30 +0100

    Removed PsGet

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,4 @@
-Import-Module PsGet
-
 $psdir="${env:USERPROFILE}\Documents\WindowsPowerShell"  
 Get-ChildItem "${psdir}\*.ps1" -Exclude $MyInvocation.MyCommand.Name | %{.$_} 
 
-. (Join-Path (Split-Path $profile) "\Modules\posh-git\profile.example.ps1")
+Import-Module posh-git
