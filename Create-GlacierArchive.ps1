8187131b407fdab3bfdd5fcaa33c491e329967f9
Boris Wilhelms <boris.wilhelms@thinktecture.com>
2017-03-25 22:09:52 +0100

    Remove not needed scripts

--- Create-GlacierArchive.ps1
+++
@@ -1,7 +0,0 @@
-﻿function Create-GlacierArchive($folder)
-{
-    $archive = Split-Path -leaf $folder
-    
-    Get-FileList $folder | Export-Csv "$($archive)-Content.csv"
-    Compress-Archive -Path $folder -DestinationPath "$($archive).zip"
-}
