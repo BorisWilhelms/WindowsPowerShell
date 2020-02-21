36dde044f09ab5603b7ef69ea998fb703a065bf8
Boris Wilhelms <mail@wille-zone.de>
2020-02-21 10:03:44 +0100

    Updates profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,13 @@
 Import-Module posh-git
 Import-Module oh-my-posh
-Import-Module Jump.Location
 Set-Theme paradox
 $DefaultUser = 'wired'
 
 $psdir = Split-Path -Path $profile  
+$env:Path += ";${psdir}\Scripts"
 Get-ChildItem "${psdir}\cmdlets\*.ps1" -Exclude $MyInvocation.MyCommand.Name | % {.$_} 
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
